5f24c53c9759b2a5b2612e92cf4bebddb698aee4
girlandomarinho <116310865+girlandomarinho@users.noreply.github.com>
2024-01-19 15:30:42 -0300

    Aprendendo JS

--- ex01/js/index.js
+++ ex01/js/index.js
@@ -1,2 +1,21 @@
-document.getElementById("texto").innerHTML="Meu primeiro texto em <b>JS</b>!";
-console.log("Oi isso é um console.log");
+/*Vamos entender Variáveis
+Variáveis são "recipientes" onde podemos armazenar informações que podem variar, ou seja, podem ter qualquer tipo de valor.
+
+No JavaScript temos 3 palavras-chaves para declarar variáveis:
+-> var
+-> let
+-> const
+*/
+
+//Declaração de variáveis
+var nome, sobrenome, nomeCompleto, idade;
+
+//Atribuição dos valores
+nome = "Girlando";
+sobrenome = "Marinho";
+idade = 18;
+nomeCompleto = nome+" "+sobrenome;
+
+pessoa = idade+nome;
+
+document.getElementById("texto").innerHTML=pessoa;
